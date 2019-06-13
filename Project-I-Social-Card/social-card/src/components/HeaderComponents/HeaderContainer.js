b9ebfe21759a06b3a9a66e8368846b972a671394
Delphina Bailey Nicols <DelphinaBaileyNicols@gmail.com>
2019-06-12 21:17:19 -0400

    In the Project-I-Social-Card folder I wrote several files first before compiling with the react server.  That was an error.  I am still debugging.  The project isn't compiled yet.

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+import './components/HeaderComponents/HeaderTitle';
+import './components/Headeromponents/HeaderContent';
+
+const HeaderContainer = ()=>{
+  return  (
+  <HeaderTitle/>
+  <HeaderContent/>
+  )
+};
+
+  export default HeaderContainer;
