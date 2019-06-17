97a669ae2ea6a0a21f036ac29cf7ec92fbd6fa43
00Delphina00 <48776696+00Delphina00@users.noreply.github.com>
2019-06-17 19:09:31 -0400

    Project-I-SocialCardMVP
    I rewrote it and debugged it on codesandbox.io

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+
+const HeaderTitle = () => {
+  return (
+    <div className="headerTitle">
+      <p className="lambdaHeaderTxt"> Lambda School </p>
+      <p className="lambdaHandle"> @LambdaSchool * </p>
+      <p className="timeStamp">17 june</p>
+    </div>
+  );
+};
+
+export default HeaderTitle;
